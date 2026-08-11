e523863f5778299c244de686f7061aacfc104c82
Eric Hole <ehole@onixnet.com>
2026-08-11 17:21:30 +0000

    chore: add license headers to new files

--- devops_bench/agents/sandbox.py
+++ devops_bench/agents/sandbox.py
@@ -1,4 +1,4 @@
-# Copyright 2026 Google LLC
+# Copyright 2026 The Kubernetes Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 """Run a CLI agent inside a container with a scoped cluster identity.
 
 WHY THIS EXISTS. The CLI agent harnesses invoke the agent binary as a plain
